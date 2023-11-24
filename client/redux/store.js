d8f30de3204cedbd92d1f06829ba3b2be80f98ac
srishtirjk <shristirjk@gmail.com>
2023-11-24 16:55:02 +0530

    add store persist

---
+++ client/redux/store.js
@@ -0,0 +1,23 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import {useReducer} from "./user/userSlice.js"
+import userReducer from './user/userSlice.js';
+import {persistStore,persistReducer} from 'redux-persist'
+import  storage from 'redux-persist/lib/storage'
+
+const rootReducer =combineReducers({user:userReducer})
+const persistConfig ={
+  key:'root',
+  storage,
+  version:1,
+}
+//this can hold the login data as locally
+const persistedReducer =persistReducer(persistConfig,rootReducer)
+export const store = configureStore({
+  // reducer:{user:useReducer},
+  reducer:persistedReducer,
+  middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
+    serializableCheck:false,
+    //in serializablecheck we can put variable in not a serial form
+  })
+})
+export  const  persistor = persistStore(store)
