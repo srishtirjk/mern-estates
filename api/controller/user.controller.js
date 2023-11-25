6c96f436fd4e679080864aea1e2bd5f6dc059b30
srishtirjk <shristirjk@gmail.com>
2023-11-25 19:41:47 +0530

    create user uupdate api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,7 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs"
+
 export const test=(req,res)=>{
     res.json({
         message:"hiiiiiiiiiiii"
@@ -5,3 +9,31 @@ export const test=(req,res)=>{
 }
 
 //this is api routing
+
+export const updateUser =async (req,res,next)=>{
+    // this const is work after verified by verifyUer.js file 
+    //then it work
+    if(req.user.id !== req.params.id)
+     return next(errorHandler(
+        401,"you can update your own account!"));
+        try{
+if(req.body.password){
+    req.body.password = bcryptjs.hashSync(req.body.password,10)
+}
+const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+    $set:{
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        avatar:req.body.avatar,
+
+    },
+},{new:true})
+const {password, ...rest}= updatedUser._doc;
+ res.status(200).json(rest);        
+}catch(error){
+       next(error,"")
+        }
+
+
+};
